d8deb268260394fb5246de7b7ad7ad0ad424cfe7
hamada.alosh <mohamad.75.alosh@gmail.com>
2022-03-08 16:29:35 +0000

    App has been cleaned up. Dataface has been expanded.

--- src/main/java/uk/co/asepstrath/bank/App.java
+++ src/main/java/uk/co/asepstrath/bank/App.java
@@ -38,7 +38,7 @@ public class App extends Jooby {
          */
         DataSource ds = require(DataSource.class);
         Logger log = getLog();
-
+        Dataface data = new Dataface(ds,log);
         mvc(new Controller(ds,log));
 
         /*
@@ -66,51 +66,10 @@ public class App extends Jooby {
     public void onStart() {
         Logger log = getLog();
         log.info("Starting Up...");
-        // Fetch DB Source
         DataSource ds = require(DataSource.class);
-        // Open Connection to DB
-        try (Connection connection = ds.getConnection()) {
-            // Initialise the connection
-            Statement stmt = connection.createStatement();
-            ArrayList<Account> accounts = new ArrayList<Account>();
-            stmt.executeUpdate("CREATE TABLE Accounts (id varchar(255), name varchar(255), balance varchar(255), currency varchar(255), accountType varchar(255), noOfTransactions varchar(255), noOfFailedTransactions varchar(255))");
-            HttpResponse<JsonNode> accountsResponse = Unirest.get("http://api.asep-strath.co.uk/api/team4/accounts").asJson();
-            JSONArray accountsArray = accountsResponse.getBody().getArray();
-            for (int i = 0; i < accountsArray.length(); i++) {
-                JSONObject tempaccount = accountsArray.getJSONObject(i);
-                stmt.addBatch("INSERT INTO Accounts " + "VALUES ('" + tempaccount.getString("id") + "', '"
-                        + SQLverify(tempaccount.getString("name")) + "', '" + tempaccount.getString("balance") +"', '"
-                        + tempaccount.getString("currency")+"', '"+tempaccount.getString("accountType")+ "','" + "0" + "','" + "0" + "')");
-                accounts.add(new Account(tempaccount.getString("id"), tempaccount.getString("name"), Float.valueOf(tempaccount.getString("balance")).floatValue(), tempaccount.getString("currency"), tempaccount.getString("accountType"), 0, 0));
-            }
-            stmt.executeBatch();
-            ArrayList<Transaction> transactions = new ArrayList<Transaction>();
-            stmt.executeUpdate("CREATE TABLE Transactions (withdrawAccount varchar(255), depositAccount varchar(255), timestamp varchar(255), id varchar(255), amount varchar(255), currency varchar(255))");
-            HttpResponse<JsonNode> transactionsResponse = Unirest.get("http://api.asep-strath.co.uk/api/team4/transactions?PageSize=999").asJson();
-            JSONArray transactionArray = transactionsResponse.getBody().getArray();
-            for (int i = 0; i < transactionArray.length(); i++) {
-                JSONObject tempaccount = transactionArray.getJSONObject(i);
-                stmt.addBatch("INSERT INTO Transactions " + "VALUES ('" + tempaccount.getString("withdrawAccount") + "', '"
-                            + tempaccount.getString("depositAccount") + "', '" + tempaccount.getString("timestamp") + "', '"
-                            + tempaccount.getString("id") + "', '" + tempaccount.getString("amount") + "', '" + tempaccount.getString("currency") + "')");
-                transactions.add(new Transaction(tempaccount.getString("withdrawAccount"), tempaccount.getString("depositAccount"), tempaccount.getString("timestamp"), tempaccount.getString("id"), Float.valueOf(tempaccount.getString("amount")), tempaccount.getString("currency")));
-            }
-            stmt.executeBatch();
-            String fraudResponse = Unirest.get("http://api.asep-strath.co.uk/api/team4/fraud").header("accept", "application/json").asString().getBody();
-            String fraudArray[] = fraudResponse.replace("[", "").replace("]", "").replaceAll("\"", "").split(",");
-            HashMap<Transaction, Account> toProcess = ProcessTransactions.processTransactions(accounts, transactions, fraudArray);
-            for(Map.Entry<Transaction, Account> set: toProcess.entrySet()) {
-                String accountBalance = String.valueOf(set.getValue().getBalance());
-                String accountNoOfTransactions = String.valueOf(set.getValue().getNoOfTransactions());
-                String accountNoOfFailedTransactions = String.valueOf(set.getValue().getNoOfFailedTransactions());
-                stmt.addBatch("UPDATE Accounts SET balance = " + accountBalance + "," + "noOfTransactions = " + accountNoOfTransactions + "," + "noOfFailedTransactions = " + accountNoOfFailedTransactions);
-            }
-
-            stmt.executeBatch();
-
-        } catch (SQLException e) {
-            log.error("Database Creation Error",e);
-        }
+        Dataface data = new Dataface(ds,log);
+        // Initialise data.
+        log.info(data.initialiseData());
     }
     /*
     This function will be called when the application shuts down
